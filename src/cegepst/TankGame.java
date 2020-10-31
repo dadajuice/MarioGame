08af4f5afd5af719b460d3981ec2de03f49fc96b
dadajuice <davidt2003@msn.com>
2020-10-31 17:05:11 -0400

    Ajout tests de collision

--- src/cegepst/TankGame.java
+++ src/cegepst/TankGame.java
@@ -5,14 +5,20 @@ import cegepst.engine.CollidableRepository;
 import cegepst.engine.Game;
 import cegepst.engine.entity.StaticEntity;
 
+import java.util.ArrayList;
+
 public class TankGame extends Game {
 
     private Tank tank;
     private GamePad gamePad;
+    private ArrayList<Brick> bricks;
 
     public TankGame() {
         gamePad = new GamePad();
         tank = new Tank(gamePad);
+        bricks = new ArrayList<>();
+        bricks.add(new Brick(250, 250));
+        bricks.add(new Brick(400, 250));
     }
 
     @Override
@@ -35,6 +41,9 @@ public class TankGame extends Game {
 
     @Override
     public void draw(Buffer buffer) {
+        for (StaticEntity entity : bricks) {
+            entity.draw(buffer);
+        }
         tank.draw(buffer);
     }
 }
